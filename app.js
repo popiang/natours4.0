1af45f15039cb8fb419a08623c541a231c3e3082
SHAHRIL MAD SHAH <popiang@hotmail.com>
2024-03-05 07:41:17 +0800

    asdf

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controller/errorController');
+const errorController = require('./controller/errorController');
 
 const app = express();
 
@@ -29,21 +32,10 @@ app.use('/api/v1/users', userRouter);
 
 // to handle all unhandled routes
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'Fail',
-        message: `Can't find ${req.originalUrl} on this server!`,
-    });
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
 });
 
 // global error handling middleware
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
